1534e97a7c767314e5f1a953f88856fca5f6016d
Emanuel Gomez <msijuicebox@gmail.com>
2024-10-13 20:45:19 -0300

    cors?????????

--- main.py
+++ main.py
@@ -16,13 +16,24 @@ app = FastAPI()
 
 
 @app.middleware("http")
-async def cors_handler(request: Request, call_next):
-    response: Response = await call_next(request)
-    response.headers["Access-Control-Allow-Credentials"] = "true"
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     return response
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/emojis", dependencies=[Depends(get_current_user)])
