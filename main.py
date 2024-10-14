e7a02331ec8bd9aa0c914c860e8f4bf699bb217a
Emanuel Gomez <msijuicebox@gmail.com>
2024-10-13 21:38:33 -0300

    wth

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import logging
 from typing import List, Union
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Request
 from dotenv import dotenv_values
-from middleware import get_current_user
+import jwt
 from fastapi.middleware.cors import CORSMiddleware
 
 config = dotenv_values("./.env")
 SECRET_KEY = config.get("SECRET_JWT")
-ALGORITHM = config.get("ALGORITHM")
 
 app = FastAPI()
 
@@ -17,6 +17,38 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+def format_token(token: str) -> str:
+    token = token.replace("-", "+").replace("_", "/")
+    return token + "=" * (4 - len(token) % 4)
+
+
+def get_current_user(request: Request):
+    auth_header = request.headers.get("Authorization")
+    if auth_header:
+        token = auth_header.split(" ")[1]
+        token = format_token(token)
+
+        try:
+            payload = jwt.decode(
+                token, SECRET_KEY, algorithms=["HS256"], audience="authenticated"
+            )
+            logging.info(f"Decoded payload: {payload}")
+            email = payload.get("user_metadata", {}).get("email")
+            role = payload.get("role")
+            if email is None or role is None:
+                raise HTTPException(status_code=403, detail="Invalid token payload")
+            return {"email": email, "role": role}
+        except jwt.ExpiredSignatureError:
+            logging.error("Token has expired")
+            raise HTTPException(status_code=403, detail="Token has expired")
+        except jwt.InvalidTokenError:
+            logging.error("Invalid JWT token")
+            raise HTTPException(status_code=403, detail="Invalid JWT token")
+    else:
+        logging.error("Authorization header missing")
+        raise HTTPException(status_code=403, detail="Authorization header missing")
 
 
 @app.get("/emojis", dependencies=[Depends(get_current_user)])
